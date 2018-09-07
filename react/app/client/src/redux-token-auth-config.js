5ce8c69530d788450580caef7c29db050512b296
Dmitriy <dr.trem86@gmail.com>
2018-09-07 16:53:19 +0300

    push heroku

--- react/app/client/src/redux-token-auth-config.js
+++ react/app/client/src/redux-token-auth-config.js
@@ -2,7 +2,7 @@ import { generateAuthActions } from 'redux-token-auth'
  
 const config = {
   authUrl: "https://blooming-dawn-18533.herokuapp.com/api/v1/auth",
-  storage: true,
+  storage: false,
   userAttributes: {
     email: 'email',
   },
